4b23f2c7affc621aee5d1bdbb0a3ac9eaeef6043
LiuKaiLong <helloworld.lkl@gmail.com>
2017-04-16 14:12:54 +0800

    debug

--- db/userSQL.js
+++ db/userSQL.js
@@ -1,6 +1,6 @@
 var UserSQL = {
-  getCourseType: 'SELECT ccID,ccName,ccOrderFlag FROM t_courseclass WHERE ccDel = 0 AND ccShowFlag = 0',
-  getAllCourseType: 'SELECT ccID,ccName,ccShowFlag,ccOrderFlag,ccDate FROM t_courseclass WHERE ccDel = 0',
+  getCourseType: 'SELECT ccID,ccName,ccOrderFlag FROM t_courseClass WHERE ccDel = 0 AND ccShowFlag = 0',
+  getAllCourseType: 'SELECT ccID,ccName,ccShowFlag,ccOrderFlag,ccDate FROM t_courseClass WHERE ccDel = 0',
   getCarousel: 'SELECT * FROM t_carousel',
   getRecommendCourse: 'SELECT t_course.cID, cName, cCover, cInfo, v_courseSectionCount.sectionCount FROM t_course, v_courseSectionCount WHERE t_course.cID = v_courseSectionCount.cID AND cShowFlag = 1',
   getCourse: 'SELECT t_course.cID, cName, cCover, cInfo, v_courseSectionCount.sectionCount FROM t_course, v_courseSectionCount WHERE t_course.cID = v_courseSectionCount.cID',
